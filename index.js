b79af4d7a8503154b0f44b959f97ed66fb1bcccd
EC2 Default User <ec2-user@ip-172-31-66-159.ec2.internal>
2023-02-24 16:39:40 +0000

    added event on start button

--- index.js
+++ index.js
@@ -1,6 +1,33 @@
-let moreinfo = document.getElementById('moreInfo')
+//How to play Button
+let moreinfo = document.getElementById('moreInfo');
 
 function pop() {
-    alert('test')
+    alert('test');
 }
-moreinfo.addEventListener('click', pop)
+moreinfo.addEventListener('click', pop);
+
+//Start Button
+let start = document.getElementById('start');
+
+start.addEventListener('click', Remove);
+
+let body = document.getElementById('body');
+
+//Remove Function
+function Remove() {
+    let bodyChildren = body.children;
+    for(let i = 0; i < bodyChildren.length;i){
+        bodyChildren[i].remove();
+    }
+}
+
+//Walking
+
+
+//function for walking
+
+function walking(){
+    
+}
+
+//
